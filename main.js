57e4955a0c9d232b8b8486756fccce7a33d1cc7d
md678685 <mathew678@hotmail.co.uk>
2017-01-22 17:39:14 +0000

    Add initial v0 route

--- main.js
+++ main.js
@@ -17,6 +17,24 @@ const express = require("express");
 // Initialise Express
 const app = express();
 
+function getStatus() {
+    let players = jcmp.players.map(player => player.name);
+    let packages = jcmp.packages.map(package => package.name);
+    return {
+        serverVersion,
+        clientVersion,
+        packages,
+        players
+    }
+}
+
+const serverVersion = jcmp.server.version || jcmp.version || config.jcmp.serverVersion;
+const clientVersion = jcmp.server.version || jcmp.version || config.jcmp.clientVersion;
+
+app.get("/v0/", (req, res) => {
+    res.send(getStatus());
+})
+
 app.listen(config.express.port, (req, res) => {
     console.log(`Justquery listening on port ${config.express.port}`);
 });
